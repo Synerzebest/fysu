a560219687fe5ef6d7840988030f1b2808ac4509
Charles Guelton <charlesguelton07@gmail.com>
2025-07-27 19:01:23 +0200

    api use supabase instead of prisma

--- pages/api/products/[slug].ts
+++ pages/api/products/[slug].ts
@@ -1,16 +1,20 @@
-import { prisma } from '@/lib/prisma'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '@/lib/supabaseClient'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { slug } = req.query
-
   if (typeof slug !== 'string') return res.status(400).json({ error: 'Invalid slug' })
 
-  const product = await prisma.product.findUnique({
-    where: { slug },
-  })
+  const { data: product, error } = await supabase
+    .from('Product')
+    .select('*')
+    .eq('slug', slug)
+    .single()
 
-  if (!product) return res.status(404).json({ error: 'Not found' })
+  if (error || !product) {
+    console.error(error)
+    return res.status(404).json({ error: 'Product not found' })
+  }
 
   return res.status(200).json(product)
 }
